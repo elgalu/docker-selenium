d60ee47a32832dbb6641f7d147e5cf4a0f5818df
Leo Gallucci <elgalu3@gmail.com>
2017-09-11 16:22:53 +0200

    Deprecate transfer-logs.sh

--- selenium-node-chrome/bin/transfer-logs.sh
+++ selenium-node-chrome/bin/transfer-logs.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-# This is currently not working well
-# if [ "${CHROME}" == "true" ]; then
-#     # Try to retrieve Chrome Browser console logs when using this image through Zalenium
-#     cp /home/seluser/chrome-user-data-dir/chrome_debug.log /var/log/cont/chrome_browser.log
-# fi
+# TODO: Delete this file
