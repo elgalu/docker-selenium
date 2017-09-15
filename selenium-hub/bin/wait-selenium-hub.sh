d57a4c231e7f1129e0f2871ff5a0009e5c2f6eab
Leo Gallucci <elgalu3@gmail.com>
2017-09-15 18:05:14 +0200

    Upgrade Selenium 3.5 new endpoints

--- selenium-hub/bin/wait-selenium-hub.sh
+++ selenium-hub/bin/wait-selenium-hub.sh
@@ -13,10 +13,10 @@ fi
 echo "Waiting for Selenium Hub to be ready..."
 
 # Selenium <= 3.3.1 then: while ! curl -s "${SEL_STATUS_URL}" | jq '.status' | grep "13"; do
-SUCESS_CMD="jq .status | grep 13"
+# SUCESS_CMD="jq .status | grep 13"
 
 # Selenium >= 3.5.0 then: while ! curl -s "${SEL_STATUS_URL}" | jq '.status' | grep "0"; do
-# SUCESS_CMD="jq .status | grep 0"
+SUCESS_CMD="jq .status | grep 0"
 
 while ! curl -s "${SEL_STATUS_URL}" | sh -c "${SUCESS_CMD}"; do
   echo -n '.'
