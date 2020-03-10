b86e23d56906b393abdc0298d5451a398745442b
void* <voidp@protonmail.com>
2020-03-10 23:24:15 +0100

    start pulse audio server only if AUDIO env is true

--- video-rec/bin/start-video-rec.sh
+++ video-rec/bin/start-video-rec.sh
@@ -45,18 +45,18 @@ else
   touch "${tmp_video_path}"
 fi
 
-# Start the pulseaudio server
-pulseaudio -D --exit-idle-time=-1
-
-# Load the virtual sink and set it as default
-pacmd load-module module-virtual-sink sink_name=v1
-pacmd set-default-sink v1
-
-# set the monitor of v1 sink to be the default source
-pacmd set-default-source v1.monitor
-
 # ffmpeg
 if  ["${AUDIO}" == "true"]; then
+  # Start the pulseaudio server
+  pulseaudio -D --exit-idle-time=-1
+
+  # Load the virtual sink and set it as default
+  pacmd load-module module-virtual-sink sink_name=v1
+  pacmd set-default-sink v1
+
+  # set the monitor of v1 sink to be the default source
+  pacmd set-default-source v1.monitor
+
   ffmpeg -r ${FFMPEG_FRAME_RATE} \
     -f "pulse" \
     -i "default" \
